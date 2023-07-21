eeb0306fe1f65eb2a40b744eadc77fc3cffb25c8
Thejas T M <121598704+thejastm15@users.noreply.github.com>
2023-07-21 20:26:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/Textform';
+
+function App() {
+  return (
+    <>
+    <Navbar title = 'textUtil'/>
+    <TextForm heading='Enter the text'/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Textform.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className="container" > 
+            <h1 className='mb-4'>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button  className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button  className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button  className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button  className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        
+
+        <h2>Preview</h2>
+        
+        <p>{text}</p>
+        </div>
+        </>
+    )
+}
