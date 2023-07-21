9a3192b27638bb0f8b80bc022e3eafe7b8993093
Thejas <thejastm15@gmail.com>
2023-07-21 20:56:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/Textform';
+// import TextForm from './components/Textform';
 
 function App() {
   return (
     <>
     <Navbar title = 'textUtil'/>
     <TextForm heading='Enter the text'/>
+    {/* <About/> */}
     </>
   )
 }
